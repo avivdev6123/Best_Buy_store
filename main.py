47101f091100fd3ddec38bb1268ba7e160375401
avivavidan <aviv6123@gmail.com>
2025-11-10 14:53:13 +0200

    third commit: Writing user interface

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 from products import Product
 from store import Store
 
+MENU = {
+    "1": "List all products in store",
+    "2": "Show total amount in store",
+    "3": "Make an order",
+    "4": "Quit"
+}
+
+def show_menu():
+    print("   Store Menu\n   ----------")
+    for item in MENU.keys():
+        print(f"{item}. {MENU[item]}")
+
+def start(store):
+    show_menu()
+
+
 def main():
+    # setup initial stock of inventory
     product_list = [Product("MacBook Air M2", price=1450, quantity=100),
                     Product("Bose QuietComfort Earbuds", price=250, quantity=500),
-                    Product("Google Pixel 7", price=500, quantity=250),
+                    Product("Google Pixel 7", price=500, quantity=250)
                     ]
-
     best_buy = Store(product_list)
-    products = best_buy.get_all_products()
-    print(best_buy.get_total_quantity())
-    print(best_buy.order([(products[0], 1), (products[1], 2)]))
 
-
+    start(best_buy)
 if __name__ == "__main__":
     main()
