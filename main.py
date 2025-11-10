73837c44b996a3bb647a8a0234a982e32852684e
avivavidan <aviv6123@gmail.com>
2025-11-10 16:25:27 +0200

    last commit: user interface done

--- main.py
+++ main.py
@@ -14,7 +14,44 @@ def show_menu():
         print(f"{item}. {MENU[item]}")
 
 def start(store):
-    show_menu()
+    while True:
+        show_menu()
+        choice = input("Please choose a number: ")
+        if choice not in MENU.keys():
+            continue
+        elif choice == "1":
+            store.show_store_catalog()
+
+        elif choice == "2":
+            print(f"Total of {store.get_total_quantity()} items in store\n")
+
+        elif choice == "3":
+            order_list = []
+            store.show_store_catalog()
+            print("When you want to finish order, enter empty text.")
+            while True:
+                product_choice = input("Which product # do you want? ")
+                product_quantity = input("What amount do you want? ")
+                if (product_choice == "" or product_choice == None):
+                    break
+
+                try:
+                    selected_index = int(product_choice)
+                    selected_quantity = int(product_quantity)
+
+                    if 1 <= selected_index <= len(store.get_all_active_products()):
+                        item_tuple = (store.products_list[selected_index-1], selected_quantity)
+                        order_list.append(item_tuple)
+                        print("Product added to list!\n")
+                    else:
+                        print("please select a product # from the list!")
+
+                except ValueError as a:
+                    print(f"Error adding product!, error message: {a}\n")
+
+            if order_list:
+                total_order_amount = store.order(order_list)
+                print(f"Order made! Total payment: {total_order_amount}\n")
 
 
 def main():
@@ -24,7 +61,7 @@ def main():
                     Product("Google Pixel 7", price=500, quantity=250)
                     ]
     best_buy = Store(product_list)
-
     start(best_buy)
+
 if __name__ == "__main__":
     main()
--- store.py
+++ store.py
@@ -19,13 +19,26 @@ class Store:
         else:
             print("the item you are trying to remove is not Product type")
 
+    def show_store_catalog(self):
+        print("------")
+        for index in range(len(self.products_list)):
+            print(f"{index+1}. {self.products_list[index].name}, Price: {self.products_list[index].price},"
+                  f" Quantity: {self.products_list[index].quantity}")
+        print("------\n")
+
+    def get_total_amount(self) -> float:
+        total_amount = 0
+        for product in self.products_list:
+            total_amount += product.price * product.quantity
+        return total_amount
+
     def get_total_quantity(self) -> int:
         total_items = 0
         for product in self.products_list:
             total_items = total_items + product.quantity
         return total_items
 
-    def get_all_products(self):
+    def get_all_active_products(self):
         active_products = []
         for product in self.products_list:
             if product.active:
@@ -34,7 +47,7 @@ class Store:
 
     def order(self, shopping_list) -> float:
         order_total_amount = 0
-        for product in self.products_list:
+        for product in self.get_all_active_products():
             for product_order, quantity_order in shopping_list:
                 if product.name == product_order.name:
                     order_total_amount = order_total_amount + product.buy(quantity_order)
